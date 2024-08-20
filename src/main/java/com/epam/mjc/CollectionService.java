b9608659345af39059af38aed4d78e785b63c949
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-20 11:40:03 +0500

    Update CollectionService.java

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -2,34 +2,41 @@ package com.epam.mjc;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        return list.stream()
-            .filter(i -> i % 2 == 0)
+return list.stream()
+        .filter(i -> i % 2 == 0)
                 .collect(Collectors.toList());
     }
 
+
     public List<String> toUpperCaseCollection(List<String> list) {
-        return list.stream()
-            .map(i -> i.toUpperCase())
-                .collector(Collectors.toList());
+return list.stream()
+        .map(i->i.toUpperCase())
+                .collect(Collectors.toList());
+
     }
+
 
     public Optional<Integer> findMax(List<Integer> list) {
         return list.stream()
-            .max(Integer::compareTo);
+        .max(Integer::compareTo);
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        return list.stream()
-            .flatMap(resultList->resultList.stream())
-            .main(Integer::compareTo);
+return list.stream()
+        .flatMap(resultList->resultList.stream())
+        .min(Integer::compareTo);
+
     }
 
     public Integer sum(List<Integer> list) {
         return list.stream()
-            .reduce(0,(a,b) -> (a+b));
+                .reduce(0,(a,b) -> (a+b));
     }
 }
+
+//можно ли заменить на форич
